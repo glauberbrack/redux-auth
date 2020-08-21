24e03dabd358e0918f2239a360c4809f56a4d88a
Glauber Brack <glauber@brack.com.br>
2020-08-21 16:54:21 -0300

    using dispatch on index

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Container, Content, AnimationContainer } from './styles';
 
 import logo from '../../assets/icon.png';
+import { StoreState } from '../../store/createStore';
+import { signInRequest } from '../../store/modules/auth/actions';
 
 const UserSignIn: React.FC = () => {
+  const { isAuthenticated } = useSelector((state: StoreState) => state.auth)
+  const dispatch = useDispatch();
+  console.log(' IS AUTHENTICATED: ', isAuthenticated)
+
   return (
     <Container>
       <Content>
@@ -14,7 +21,7 @@ const UserSignIn: React.FC = () => {
           <div className="sign-in-page">
             <input type="text" defaultValue="glauber@brack.com.br" />
             <input type="password" defaultValue="12345678" />
-            <button>Login</button>
+            <button onClick={() => dispatch(signInRequest({ email: 'glauber@brack.com.br', password: '12345678'}))}>Login</button>
           </div>
 
         </AnimationContainer>
